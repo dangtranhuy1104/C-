55d28e5fd72cf0ad808ca35a9e58561c6f14e83e
dangtranhuy1104 <dangtranhuy1104@gmail.com>
2021-08-30 23:25:32 +0700

    LabGroup

---
+++ LabGroup/Car/Seat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine Start";
+        }
+
+        public string Stop()
+        {
+            return "Break!!!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} " + "Seat " + $"{Model}" +
+                $"{Start()} " + "\n" + $"{Stop()}";
+        }
+    }
+}
+
---
+++ LabGroup/Car/Tesla.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class Tesla : ICar, IElectricCar
+    {
+        public Tesla() { }
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+
+        }
+
+        public string Model
+        {
+            get;
+            set;
+        }
+
+        public string Color
+        {
+            get;
+            set;
+        }
+
+        public int Battery { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} " + "Tesla " + $"{Model} " + "with " + $"{Battery} " + "Batteries" + "\n" +
+                $"{Start()} " + "\n" + $"{Stop()}";
+        }
+
+    }
+}
