b7d766ea101865012be0d863ab2ff77e9c51a668
dangtranhuy1104 <dangtranhuy1104@gmail.com>
2021-08-31 09:48:46 +0700

    LapGroup

---
+++ LapGroup/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter radius: ");
+            var radius = Convert.ToInt32(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+            circle.Draw();
+
+
+            Console.Write("Enter width: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter height: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height);
+            rect.Draw();
+
+        }
+    }
+}
---
+++ LapGroup/ConsoleApp1/Ferrary/IFerrari.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferrari
+{
+    interface IFerrari
+    {
+        string Model { get; }
+        string Driver { get; }
+        string UseBrakes();
+        string PushGasPedal();
+    }
+}
