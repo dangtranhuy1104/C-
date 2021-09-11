c6666e187cbb1c4e17eaf08748598555a442a61a
dangtranhuy1104 <dangtranhuy1104@gmail.com>
2021-09-11 10:50:44 +0700

    Bai thi C#

---
+++ ThiCsharp/ThiCsharp/ManageProudct.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThiCsharp
+{
+    public class ProductManagement
+    {
+        public List<Product> ListProduct = null;
+
+
+        public ProductManagement()
+        {
+            ListProduct = new List<Product>();
+        }
+
+        public int GenerateID()
+        {
+            int max = 1;
+            if (ListProduct != null && ListProduct.Count > 0)
+            {
+                max = ListProduct[0].STT;
+                foreach (Product pr in ListProduct)
+                {
+                    if (max < pr.STT)
+                    {
+                        max = pr.STT;
+                    }
+                }
+                max++;
+            }
+            return max;
+        }
+
+        public int CountProduct()
+        {
+            int count = 0;
+            if (ListProduct != null)
+            {
+                count = ListProduct.Count;
+            }
+            return count;
+        }
+
+        //Ham tim theo ma SP
+        public Product FindByProductID(string id)
+        {
+            Product searchID = null;
+            if (ListProduct != null && ListProduct.Count > 0)
+            {
+                foreach (Product pro in ListProduct)
+                {
+                    if (pro.ProductID == id)
+                    {
+                        searchID = pro;
+                    }
+                }
+            }
+            return searchID;
+        }
+
+        public void CreateProduct()
+        {
+            Product pr = new Product();
+            pr.STT = GenerateID();
+
+            Console.Write("Nhap ID san pham : ");
+            pr.ProductID = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Nhap ten san pham : ");
+            pr.Name = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Nhap gia san pham: ");
+            pr.Price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("   Ban co muon luu san pham nay (Y/N) : ");
+            string save = Convert.ToString(Console.ReadLine());
+            if (save == "Y" || save == "y" || save == "yes" || save == "YES")
+            {
+                Console.Clear();
+                ListProduct.Add(pr);
+                Console.WriteLine("Them san pham thanh cong !\n");
+            }
+            else if (save == "N" || save == "n" || save == "no" || save == "NO")
+            {
+                Console.Clear();
+                Console.WriteLine(" San pham khong duoc luu lai !\n\n");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Dung tao san pham!\n\n");
+            }
+        }
+
+        //Ham xoa Product theo ID
+        public bool DeleteByID(string id)
+        {
+            bool IsDeleted = false;
+            Product prID = FindByProductID(id);
+            if (prID != null)
+            {
+                IsDeleted = ListProduct.Remove(prID);
+            }
+            return IsDeleted;
+        }
+
+        // Ham hien thi danh sach Product
+        public void ShowProduct(List<Product> products)
+        {
+            Console.WriteLine("{0,-5} | {1,-15} | {2,-15} | {3,-15}  |", "No.", "ID SP", "Ten SP", "Gia");
+
+            if (products != null && products.Count > 0)
+            {
+                foreach (Product pr in products)
+                {
+                    Console.WriteLine("{0,-5} | {1,-15} | {2,-15} | ${3,-15} |", pr.STT, pr.ProductID, pr.Name, pr.Price);
+
+                }
+            }
+            Console.WriteLine();
+        }
+
+        //Ham tra ve danh sach Product
+        public List<Product> GetProducts()
+        {
+            return ListProduct;
+        }
+
+    }
+}
---
+++ ThiCsharp/ThiCsharp/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThiCsharp
+{
+    class Program
+    {
+        ProductManagement prManagement = new ProductManagement();
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Menu();
+        }
+
+        static void PressAnyKey()
+        {
+            Console.WriteLine("\n\t\tEnter any key to continue ... ");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public void Menu()
+        {
+            int key;
+            string answer;
+            while (true)
+            {
+                Console.WriteLine(" ------------- Menu -------------");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("1. Them san pham moi");
+                Console.WriteLine("2. Hien thi danh sach san pham");
+                Console.WriteLine("3. Xoa san pham theo ID ");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine(" ====================================");
+                try
+                {
+                    Console.Write(" Nhap lua chon cua ban : ");
+                    key = Convert.ToInt32(Console.ReadLine());
+
+                    switch (key)
+                    {
+                        case 1:
+                            Console.WriteLine(" Them san pham \n");
+                            prManagement.CreateProduct();
+                            Console.Write("Ban co muon tiep tuc (Y/N) :");
+                            answer = Convert.ToString(Console.ReadLine());
+                            if (answer == "Y" || answer == "y")
+                            {
+                                Console.Clear();
+                                goto case 1;
+                            }
+                            else if (answer == "N" || answer == "n")
+                            {
+                                Console.Clear();
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Lua chon cua ban khong hop len => Thoat!\n\n");
+                            }
+                            break;
+
+                        case 2:
+                            if (prManagement.CountProduct() > 0)
+                            {
+                                Console.WriteLine(" Hien thi danh sach san pham \n");
+                                prManagement.ShowProduct(prManagement.GetProducts());
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine(" Danh sach cua ban hien nay dang trong ! \n\n");
+                            }
+                            PressAnyKey();
+                            break;
+
+                        case 3:
+                            if (prManagement.CountProduct() > 0)
+                            {
+                                string id;
+                                Console.WriteLine(" Xoa san pham theo ID \n");
+                                Console.Write("Nhap ID san pham : ");
+                                id = Console.ReadLine();
+                                if (prManagement.DeleteByID(id))
+                                {
+                                    Console.WriteLine("ID san pham = {0} da duoc xoa! \n\n", id);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("San pham da bi xoa hoac khong ton tai trong danh sach\n\n");
+                                }
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Danh sach san pham trong !\n\n");
+                            }
+                            Console.Write("Ban co muon tiep uc (Y/N) :");
+                            answer = Convert.ToString(Console.ReadLine());
+                            if (answer == "Y" || answer == "y")
+                            {
+                                Console.Clear();
+                                goto case 3;
+                            }
+                            else if (answer == "N" || answer == "n")
+                            {
+                                Console.Clear();
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Lua chon cua ban khong hop len => Thoat!\n\n");
+                            }
+                            break;
+
+                        case 4:
+                            Console.WriteLine(" \u263A \u263A \u263A Goodbye !! \u263A \u263A \u263A\n\n");
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Console.Clear();
+                            Console.WriteLine(" Chuc nang khong hop le => Hay nhap lai!");
+                            break;
+
+                    }
+
+                }
+                catch (Exception error)
+                {
+                    Console.Clear();
+                    Console.WriteLine(" Loai du lieu ko hop le!!!");
+                    Console.WriteLine("=> " + error.Message);
+                }
+            }
+        }
+
+    }
+}
